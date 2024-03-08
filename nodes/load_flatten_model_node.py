85d72127168c07f272239f3435766ccbdfd856b7
logtd <logoftd@gmail.com>
2024-03-07 20:19:33 -0700

    fix

--- nodes/load_flatten_model_node.py
+++ nodes/load_flatten_model_node.py
@@ -1,5 +1,6 @@
 import torch
 from einops import rearrange
+from comfy.ldm.modules.encoders.noise_aug_modules import CLIPEmbeddingNoiseAugmentation
 import comfy.sd
 import comfy.model_base
 import folder_paths
@@ -10,6 +11,35 @@ from ..modules.unet import UNetModel as FlattenModel
 class PatchBaseModel(comfy.model_base.BaseModel):
     def __init__(self, model_config, *args, model_type=comfy.model_base.ModelType.EPS, device=None, unet_model=FlattenModel, **kwargs):
         super().__init__(model_config, model_type, device, FlattenModel)
+
+
+class PatchSDXL(PatchBaseModel):
+    def __init__(self, model_config, model_type=ModelType.EPS, device=None):
+        super().__init__(model_config, model_type, device=device)
+        self.embedder = openaimodel.Timestep(256)
+        self.noise_augmentor = CLIPEmbeddingNoiseAugmentation(
+            **{"noise_schedule_config": {"timesteps": 1000, "beta_schedule": "squaredcos_cap_v2"}, "timestep_dim": 1280})
+
+    def encode_adm(self, **kwargs):
+        clip_pooled = comfy.model_base.sdxl_pooled(
+            kwargs, self.noise_augmentor)
+        width = kwargs.get("width", 768)
+        height = kwargs.get("height", 768)
+        crop_w = kwargs.get("crop_w", 0)
+        crop_h = kwargs.get("crop_h", 0)
+        target_width = kwargs.get("target_width", width)
+        target_height = kwargs.get("target_height", height)
+
+        out = []
+        out.append(self.embedder(torch.Tensor([height])))
+        out.append(self.embedder(torch.Tensor([width])))
+        out.append(self.embedder(torch.Tensor([crop_h])))
+        out.append(self.embedder(torch.Tensor([crop_w])))
+        out.append(self.embedder(torch.Tensor([target_height])))
+        out.append(self.embedder(torch.Tensor([target_width])))
+        flat = torch.flatten(torch.cat(out)).unsqueeze(
+            dim=0).repeat(clip_pooled.shape[0], 1)
+        return torch.cat((clip_pooled.to(flat.device), flat), dim=1)
 
 
 class FlattenCheckpointLoaderNode:
@@ -24,11 +54,14 @@ class FlattenCheckpointLoaderNode:
 
     def load_checkpoint(self, ckpt_name, output_vae=True, output_clip=True):
         original_base = comfy.model_base.BaseModel
+        original_sdxl = comfy.model_base.SDXL
         comfy.model_base.BaseModel = PatchBaseModel
+        comfy.model_base.SDXL = PatchSDXL
         ckpt_path = folder_paths.get_full_path("checkpoints", ckpt_name)
         out = comfy.sd.load_checkpoint_guess_config(
             ckpt_path, output_vae=True, output_clip=True, embedding_directory=folder_paths.get_folder_paths("embeddings"))
         comfy.model_base.BaseModel = original_base
+        comfy.model_base.SDXL = original_sdxl
 
         def model_function_wrapper(apply_model_func, apply_params):
             # Prepare 3D latent
